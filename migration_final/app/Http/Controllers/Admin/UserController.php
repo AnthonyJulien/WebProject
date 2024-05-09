c745ea6d90e5ccb41b75ad1d52cfc6f1b927c812
voltairebouharb <132397014+voltairebouharb@users.noreply.github.com>
2024-05-10 00:07:53 +0200

    Revert "Update UserController.php"
    This reverts commit 849c0b76c32744f7f467ba1d00b14b6e1c096f8c.

--- migration_final/app/Http/Controllers/Admin/UserController.php
+++ migration_final/app/Http/Controllers/Admin/UserController.php
@@ -10,14 +10,14 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-       
+    // Render admin/users/users.blade.php page in the Admin Panel    
     public function users() {
-       
+        // Correcting issues in the Skydash Admin Panel Sidebar using Session
         Session::put('page', 'users');
 
 
         $users = User::get()->toArray();
-        
+        // dd($users);
 
 
         return view('admin.users.users')->with(compact('users'));
@@ -25,21 +25,21 @@ class UserController extends Controller
 
 
 
-       
+    // Update User Status (active/inactive) via AJAX in admin/users/users.blade.php, check admin/js/custom.js    
     public function updateUserStatus(Request $request) {
-        if ($request->ajax()) { 
-            $data = $request->all(); 
-            
+        if ($request->ajax()) { // if the request is coming via an AJAX call
+            $data = $request->all(); // Getting the name/value pairs array that are sent from the AJAX request (AJAX call)
+            // dd($data);
 
-            if ($data['status'] == 'Active') { 
+            if ($data['status'] == 'Active') { // $data['status'] comes from the 'data' object inside the $.ajax() method    // reverse the 'status' from (ative/inactive) 0 to 1 and 1 to 0 (and vice versa)
                 $status = 0;
             } else {
                 $status = 1;
             }
 
-            User::where('id', $data['user_id'])->update(['status' => $status]); 
+            User::where('id', $data['user_id'])->update(['status' => $status]); // $data['user_id'] comes from the 'data' object inside the $.ajax() method
 
-            return response()->json([ 
+            return response()->json([ // JSON Responses: https://laravel.com/docs/9.x/responses#json-responses
                 'status'  => $status,
                 'user_id' => $data['user_id']
             ]);
@@ -47,19 +47,19 @@ class UserController extends Controller
     }
 
 
-    
+// reset User password  via AJAX in admin/users/users.blade.php, check admin/js/custom.js    
 public function resetUserPassword(Request $request) {
-    if ($request->ajax()) { 
-        $data = $request->all(); 
-        
+    if ($request->ajax()) { // if the request is coming via an AJAX call
+        $data = $request->all(); // Getting the name/value pairs array that are sent from the AJAX request (AJAX call)
+        // dd($data);
 
         $new_password=$data['new_password'] ;
 
-        $new_password_hashed = bcrypt($new_password); 
+        $new_password_hashed = bcrypt($new_password); // storing the Reseted HASH-ed password (not the original reset password password) in the database    // bcrypt(): https://laravel.com/docs/9.x/helpers#method-bcrypt
 
-        User::where('id', $data['user_id'])->update(['password' => $new_password_hashed]); 
+        User::where('id', $data['user_id'])->update(['password' => $new_password_hashed]); // $data['user_id'] comes from the 'data' object inside the $.ajax() method
         
-        return response()->json([ 
+        return response()->json([ // JSON Responses: https://laravel.com/docs/9.x/responses#json-responses
             'user_id' => $data['user_id']
         ]);
     }
